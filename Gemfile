d0990fcc35006c641d40579cb69a6698eab1d05f
Ryan Waldron <rew@erebor.com>
2012-01-22 00:32:53 -0600

    Add forem gem and generate forum db, wire in route, add link in app layout (still lacking user auth stuff to give user permissions to read forums, etc.)

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ source 'http://rubygems.org'
 gem 'rails', '3.1.3'
 gem 'mysql2'
 
-gem 'forem'
+gem 'forem', :git => "git://github.com/radar/forem.git"
 
 gem 'omniauth'
 gem 'omniauth-twitter'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
+
+  def forem_user
+    current_user
+  end
+  helper_method :forem_user
+
   protect_from_forgery
   
   before_filter :check_profile
